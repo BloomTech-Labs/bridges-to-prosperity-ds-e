c4f7b8291065e95cdeb9b3bbe3b45e821a065dbe
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-11-14 13:27:17 -0500

    created knn visual

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import sql, csv_api 
+from app.api import sql, csv_api, viz_noah 
 
 app = FastAPI(
     title='Labs28-Team-Spencer',
@@ -13,6 +13,7 @@ app = FastAPI(
 
 app.include_router(sql.router)
 app.include_router(csv_api.router)
+app.include_router(viz_noah.router)
 
 app.add_middleware(
     CORSMiddleware,
