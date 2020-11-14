0f5db7c13c26a081e981f9752cbfbddec26f61e5
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-11-14 13:37:55 -0500

    rsolving merge conflict in main.py

--- project/app/main.py
+++ project/app/main.py
@@ -2,12 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-<<<<<<< HEAD
 from app.api import sql, csv_api, viz_noah 
-=======
-from app.api import sql, csv_api, viz 
-
->>>>>>> 89615138b7a091262834a29ab2fcc04ef4964101
 
 app = FastAPI(
     title='Labs28-Team-Spencer',
@@ -18,11 +13,7 @@ app = FastAPI(
 
 app.include_router(sql.router)
 app.include_router(csv_api.router)
-<<<<<<< HEAD
 app.include_router(viz_noah.router)
-=======
-app.include_router(viz.router)
->>>>>>> 89615138b7a091262834a29ab2fcc04ef4964101
 
 app.add_middleware(
     CORSMiddleware,
