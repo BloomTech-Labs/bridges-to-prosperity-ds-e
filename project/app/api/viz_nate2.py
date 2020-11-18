88ff846d4d89f210765ad80c754d0fa4afa1741d
Nate Dow <ndow33@gmail.com>
2020-11-18 09:44:48 -0700

    update viz_nate2.py so that the current bridge site will show an orange dot that stands out amongst the rest of the blue dots

--- project/app/api/viz_nate2.py
+++ project/app/api/viz_nate2.py
@@ -19,7 +19,7 @@ PASSWORD=os.getenv("PASSWORD")
 PORT=os.getenv("PORT")
 
 @router.get('/viz_nate2/{ind_served_impact}')
-async def vizn2():
+async def vizn2(project_code: str):
     '''
     Returns a plotly scatterplot of the overall impact score by the number of individuals served.  
 
@@ -33,37 +33,91 @@ async def vizn2():
     cursor = conn.cursor()
 
     # query the database and close the connection
-    query = f"""SELECT * FROM "impact" """
+    query = f"""SELECT * FROM "impact";"""
+    query2 = f"""SELECT * FROM "impact" WHERE project_code = '{project_code}'"""
+
     cursor.execute(query)
     result = cursor.fetchall()
+
+    cursor.execute(query2)
+    result2 = cursor.fetchall()
+
     conn.close()
 
-    # create a dataframe from the result
+    # create a dataframe of all of the data from the first result
     columns = ['project_code', 'ind_directly_served', 'days_flooded_per_year', 'market_blocked',
         'education_blocked', 'healthcare_blocked', 'river_deaths_3_yrs', 'river_inj_3yrs', 'impact_score', 
         'health_impact', 'education_impact', 'market_impact']
     df = pd.DataFrame(result, columns=columns)
 
-    # cast values to floats
-    '''df['impact_score'] = float(df['impact_score'])
-    df['ind_directly_served'] = float(df['ind_directly_served'])'''
-    counter = 0
-    for value in df['impact_score']:
-        df['impact_score'][counter] = float(df['impact_score'][counter])
-        df['ind_directly_served'][counter] = float(df['ind_directly_served'][counter])
-        counter += 1
+    # create a dataframe of just the single bridge site from the give project code
+    single_df = pd.DataFrame(result2, columns=columns)
 
-    # generate scatterplot
+
+    columns = ['ind_directly_served',
+            'days_flooded_per_year', 
+            'river_deaths_3_yrs',
+            'river_inj_3yrs',
+            'impact_score'] 
+
+    values = []
+
+    # get the values from the individual bridge site
+    for feature in columns:
+        value = single_df[feature][0]
+        value = value[:5]
+        value = float(value)
+        values.append(value)
+
+    # make a new df that will show the single bridge site on our scatterplot
+    columns = ['Individuals Served', 'Days Per Year River is Flooded',
+            'River crossing deaths in last 3 years', 
+            'River crossing injuries in last 3 years', 
+            'Overall Impact Score']
+    new_c = {'Category':columns, 'Actual':values}
+    new_df = pd.DataFrame(data=new_c)
+
+    # this adds all of the blue dots to the scatterplot
     fig = go.Figure(go.Scatter(
-            x=df["impact_score"], 
-            y=df['ind_directly_served'],
-            name='Actual',
-            orientation='h',
-            mode = 'markers',
-            marker=dict(
-                color='rgba(0, 158, 228, 1)'),
-                text=df['project_code']
-            ))
+                x=df["impact_score"], 
+                y=df["ind_directly_served"],
+                name='Other Sites',
+                orientation='h',
+                mode = 'markers',
+                marker=dict(
+                color='rgba(0, 158, 228, 1)',
+                line=dict(
+                    color='Black',
+                    width=.5)),
+                text=df['project_code'],
+                hovertemplate = "<b>Project Code: %{text}</b><br><br>" +
+                "Individuals Directly Served: %{y}<br><br>" +
+                "Overall Impact Score: %{x}"
+                ))
+
+    # add the orange dot
+    fig.add_trace(
+        go.Scatter(
+            
+            mode='markers',
+            x=[values[4]],
+            y=[values[0]],
+            name='Current Site',
+            marker = dict(
+                color='rgba(250, 140, 48, 1)',
+                size = 15,
+                line=dict(
+                    color='Black',
+                    width=2
+                )
+            
+            ),
+        text=df['project_code'],
+        hovertemplate = "<b>Project Code: %{text}</b><br><br>" +
+            "Individuals Directly Served: %{y}<br><br>" +
+            "Overall Impact Score: %{x}"  
+        )
+    )
 
     fig.update_xaxes(
             title_text = "Overall Impact Score",
